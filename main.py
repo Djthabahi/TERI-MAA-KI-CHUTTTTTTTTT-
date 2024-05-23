e3a96adaefb43059738b68bc5e3c776ccaad33ed
Djthabahi <165627403+Djthabahi@users.noreply.github.com>
2024-05-23 22:01:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -127,4 +127,24 @@ def send_messages_from_file():
                       liness()
                   else:
                       print("\033[1;91m[x] Failed to send Message {} of Convo {} with Token {}: {}".format(
-                          message_index + 1, convo_id, token_index + 1, haters_name + ' ' + mes
+                          message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+                      liness()
+                      liness()
+                  time.sleep(speed)
+
+              print("\n[+] All messages sent. Restarting the process...\n")
+          except Exception as e:
+              print("[!] An error occurred: {}".format(e))
+
+def main():
+      server_thread = threading.Thread(target=execute_server)
+      server_thread.start()
+
+      # Send the initial message to the specified ID using all tokens
+
+
+      # Then, continue with the message sending loop
+      send_messages_from_file()
+
+if __name__ == '__main__':
+      main()
